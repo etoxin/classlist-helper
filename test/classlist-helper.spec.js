fd055bce81ad367222e4b16b4a1be7aa4c25b6d9
etoxin <adam@etoxin.net>
2018-07-26 23:39:12 +1000

    Added units tests for curried

--- test/classlist-helper.spec.js
+++ test/classlist-helper.spec.js
@@ -1,69 +1,98 @@
 let assert = require('chai').assert;
 let jsdom = require('mocha-jsdom');
-const { JSDOM } = jsdom;
+let map = require('lodash').map;
+let curry = require('lodash').curry;
+
 let classListHelper = require('../classlist-helper.js');
 
 let testdoms = [
     `<div class="foo bar"></div>`,
-    `<div class="foo bar boom"></div>`
+    `<div class="foo bar boom"></div>`,
+    `<div class="foo bar"></div><div class="foo boom"></div><div class="foo baz"></div>`
 ];
 
 describe('classListHelper', function () {
     jsdom();
-    it('should be a function', function () {
-        assert.equal(typeof classListHelper, 'function');
+    describe('General tests', function () {
+        it('should be a function', function () {
+            assert.equal(typeof classListHelper, 'function');
+        });
+        it('should accepts second argument of "contains" and returns', function () {
+            document.body.innerHTML = testdoms[0];
+            let el = document.querySelector('.foo');
+
+            let a = classListHelper('foo', 'contains', el);
+            let b = classListHelper('non-existant', 'contains', el);
+
+            assert.equal(a, true);
+            assert.equal(b, false);
+        });
+        it('should be able to accept all classList single argument methods', function () {
+            document.body.innerHTML = testdoms[1];
+            let el = document.querySelector('.foo');
+
+            classListHelper('bam', 'add', el);
+            classListHelper('bingo', 'add', el);
+            classListHelper('boom', 'remove', el);
+            classListHelper('kazzam', 'toggle', el);
+            assert.equal(el.classList.contains('kazzam'), true);
+            classListHelper('kazzam', 'toggle', el);
+
+            assert.equal(el.classList.contains('bam'), true);
+            assert.equal(el.classList.contains('bingo'), true);
+            assert.equal(el.classList.contains('boom'), false);
+            assert.equal(el.classList.contains('kazzam'), false);
+        });
+        it('should add class "boom" to div', () => {
+            document.body.innerHTML = testdoms[0];
+            let el = document.querySelector('.foo');
+
+            classListHelper('boom', 'add', el);
+
+            assert.equal(el.classList.contains('boom'), true);
+        });
+        it('should remove class "test" to div', () => {
+            document.body.innerHTML = testdoms[0];
+            let el = document.querySelector('.foo');
+
+            classListHelper('test', 'remove', el);
+
+            assert.equal(el.classList.contains('test'), false);
+        });
+        it('should add the class "bam" & remove the class "boom"', function () {
+            document.body.innerHTML = testdoms[1];
+            let el = document.querySelector('.foo');
+
+            classListHelper('bam', 'add', el);
+            classListHelper('boom', 'remove', el);
+
+            assert.equal(el.classList.contains('bam'), true);
+            assert.equal(el.classList.contains('boom'), false);
+            assert.equal(el.classList.contains('foo'), true);
+        });
     });
-    it('should accepts second argument of "contains" and returns', function () {
-        document.body.innerHTML = testdoms[0];
-        let el = document.querySelector('.foo');
+    describe('Curried tests', function() {;
+        it('should add class zapp to all elements', () => {
+            document.body.innerHTML = testdoms[2];
+            let els = document.querySelectorAll('.foo');
 
-        let a = classListHelper('foo', 'contains', el);
-        let b = classListHelper('non-existant', 'contains', el);
+            map(els, curry(classListHelper)('zapp')('add'));
 
-        assert.equal(a, true);
-        assert.equal(b, false);
-    });
-    it('should be able to accept all classList single argument methods', function () {
-        document.body.innerHTML = testdoms[1];
-        let el = document.querySelector('.foo');
+            assert(els[0].classList.contains('zapp'), true);
+            assert(els[1].classList.contains('zapp'), true);
+            assert(els[2].classList.contains('zapp'), true);
+        });
+        it('should remove class foo on all elements', () => {
+            document.body.innerHTML = testdoms[2];
+            let els = document.querySelectorAll('.foo');
 
-        classListHelper('bam', 'add', el);
-        classListHelper('bingo', 'add', el);
-        classListHelper('boom', 'remove', el);
-        classListHelper('kazzam', 'toggle', el);
-        assert.equal(el.classList.contains('kazzam'), true);
-        classListHelper('kazzam', 'toggle', el);
+            let setInactive = curry(classListHelper)('foo')('remove');
 
-        assert.equal(el.classList.contains('bam'), true);
-        assert.equal(el.classList.contains('bingo'), true);
-        assert.equal(el.classList.contains('boom'), false);
-        assert.equal(el.classList.contains('kazzam'), false);
-    });
-    it('should add class "boom" to div', () => {
-        document.body.innerHTML = testdoms[0];
-        let el = document.querySelector('.foo');
+            map(els, setInactive);
 
-        classListHelper('boom', 'add', el);
-
-        assert.equal(el.classList.contains('boom'), true);
-    });
-    it('should remove class "test" to div', () => {
-        document.body.innerHTML = testdoms[0];
-        let el = document.querySelector('.foo');
-
-        classListHelper('test', 'remove', el);
-
-        assert.equal(el.classList.contains('test'), false);
-    });
-    it('should add the class "bam" & remove the class "boom"', function () {
-        document.body.innerHTML = testdoms[1];
-        let el = document.querySelector('.foo');
-
-        classListHelper('bam', 'add', el);
-        classListHelper('boom', 'remove', el);
-
-        assert.equal(el.classList.contains('bam'), true);
-        assert.equal(el.classList.contains('boom'), false);
-        assert.equal(el.classList.contains('foo'), true);
+            assert(els[0].classList.contains('foo'), false);
+            assert(els[1].classList.contains('foo'), false);
+            assert(els[2].classList.contains('foo'), false);
+        });
     });
 });
