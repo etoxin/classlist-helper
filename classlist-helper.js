1593cb0366d41d1e1988495178d055bc07a7d8a6
etoxin <adam@etoxin.net>
2018-07-30 16:53:29 +1000

    Updated method so it can accept Arrays.

--- classlist-helper.js
+++ classlist-helper.js
@@ -1,12 +1,16 @@
 /**
  * ClassList helper
- * @param {String} className the class you want to add,remove from a HTMLElement.
+ * @param {String|Array} className the class you want to add,remove from a HTMLElement.
  * @param {String} method the classList method you want to use
  * @param {HTMLElement} element that you want to manipluate the class on.
  * @returns {*}
  */
 function classlistHelper(className, method, element) {
-  return element.classList[method](className);
+    if (Array.isArray(className)) {
+        return element.classList[method](...className);
+    } else {
+        return element.classList[method](className);
+    }
 }
 
 module.exports = classlistHelper;
